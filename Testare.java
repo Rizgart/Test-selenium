1834e7ccf4c22f980a87c11ea5ccc7214bd26ac5
Rizgart <49570350+Rizgart@users.noreply.github.com>
2019-04-13 00:00:30 +0200

    Add files via upload

---
+++ Testare.java
@@ -0,0 +1,42 @@
+package config;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testare {
+	
+	public static WebDriver driver;
+	
+	public static void openBrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nozad\\Downloads\\Ny mapp\\Ny mapp\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	public static void navigate() throws Exception {
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(2000);
+	}
+	public static void click_MyAccount() {
+		driver.findElement(By.id("email")).sendKeys("kvart55@hotmail.com");
+	}
+	public static void input_Password() {
+		driver.findElement(By.id("pass")).sendKeys("V12345678");
+	}
+	public static void click_Login() {
+		driver.findElement(By.id("loginbutton")).click();
+	}
+	public static void waitfor() throws Exception {
+		Thread.sleep(2000);
+	}
+	public static void click_UserNav() {
+		driver.findElement(By.id("userNavigationLabel")).click();
+	}
+	public static void click_LogOut() {
+		driver.findElement(By.partialLinkText("Logga ut")).click();
+	}
+	
+	
+
+}
